96558d2561917ee722d0c309b71bcbcebbab0dfb
GuidtJ <jeromeguidt@hotmail.fr>
2023-06-05 16:17:55 +0200

    transition des images sur le catalogue

--- js/script.js
+++ js/script.js
@@ -105,3 +105,17 @@ toggleButton.addEventListener('click', toggleDarkMode);
 //     }
 //   });
 // });
+
+function openModal(image) {
+  var modal = document.getElementById("modal");
+  var modalImage = document.getElementById("modal-image");
+  modal.style.display = "block";
+  modalImage.src = image.src;
+}
+
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+
